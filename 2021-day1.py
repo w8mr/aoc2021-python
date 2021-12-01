33bf7cba15c80019bcba939c9d1e7936486e418c
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2021-12-01 08:45:47 +0100

    feat: functional solution Day1

--- 2021-day1.py
+++ 2021-day1.py
@@ -1,33 +1,15 @@
+import itertools
+
 from pyparsing import *
 
+def drop(it, n):
+    return itertools.islice(it, n, None)
+
+def solve(i, n):
+    return len(list(filter(lambda nm:  nm[1] < nm[0], zip(drop(i,n), i))))
 
 with open('input/2021/day1.txt') as f:
     i = list(map(int,f))
-    old = i[0]
-    print (i[0])
-    count = 0
-    for d in i:
-        if old<d:
-            count = count + 1
-        old = d
-        print(str(d)+" "+str(count))
-
-    print(count)
-
-    old1 = i[0]
-    old2 = i[1]
-    old3 = i[2]
-    print (i[0])
-    count = 0
-    for d in i:
-        if old1<d:
-            count = count + 1
-        old1 = old2
-        old2 = old3
-        old3 = d
-        print(str(d)+" "+str(count))
-
-    print(count)
-
-
-
+    
+print(solve(i,1))
+print(solve(i,3))
