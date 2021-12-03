7e4e358a6e7c64a3254bacfa3bb6db3dc2305660
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2021-12-04 00:05:23 +0100

    feat: day3 part 2

--- 2021-day3.py
+++ 2021-day3.py
@@ -1,7 +1,7 @@
 import itertools
 
 from pyparsing import *
-from functools import reduce
+from functools import partial
 import array
 
 def count(i):
@@ -12,23 +12,31 @@ def count(i):
                 a[index] += 1;
     return a
 
-def most(i):
-    return "1" if i*2 > c else "0"
-
-def least(i):
-    return "1" if i*2 < c else "0"
-
 with open('input/2021/day3.txt') as f:
     i = list(f)
 
+def part1(f):
+    a = count(i)
+    c = len(i)
+    return int("".join(map(partial(f,c), a)), 2)
 
-a = count(i)
-    
-c = len(i)
-
-        
-gamma = int("".join(map(most,a)),2)
-epsilon = int("".join(map(least,a)),2)
+gamma = part1(lambda c, i: "1" if i*2 > c else "0")
+epsilon = part1(lambda c, i: "1" if i*2 < c else "0")
 
 print (gamma*epsilon)
 
+def part2(i, f):
+    bp=0
+    while len(i)>1:
+        a = count(i)
+        c = len(i)
+        b = f(a[bp], c)
+        #print(", ".join([str(bp), str(a), str(c), str(b)]))
+        i = list(filter(lambda x: x[bp] == b, i))
+        bp += 1
+    return int("".join(i[0]),2)
+
+oxygen = part2(i, lambda i,c: "1" if i*2 >= c else "0")
+co2 = part2(i, lambda i,c: "1" if i*2 < c else "0")
+
+print(oxygen * co2)
