bd3d70326670e8f8ca8e4e8be1986fef1f84e760
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2021-12-06 10:13:29 +0100

    feat: day6 part 2

--- 2021-day6.py
+++ 2021-day6.py
@@ -1,20 +1,23 @@
-import functools
-import itertools
-from pyparsing import *
-from functools import reduce
-import array
+import collections
 
 with open('input/2021/day6.txt') as f:
     i = list(map(int,list(f)[0][:-1].split(",")))
 
-flat_map = lambda f, xs: [y for ys in xs for y in f(ys)]
+step = lambda i: i[1:7]+[i[0]+i[7],i[8],i[0]]
 
-
-def step(i):
-    return flat_map(lambda x: [x-1] if x > 0 else [6,8], i)
+d = counter=collections.Counter(i)
+#print(d)
+e=[d[k] for k in range(9)]
+#print(e)
 
 for c in range(80):
-  #  print(i)
-    i = step(i)
+#    print([e, sum(e)])
+    e = step(e)
     
-print(len(i))
+print(sum(e))
+
+for c in range(80,256):
+    #    print([e, sum(e)])
+    e = step(e)
+
+print(sum(e))
